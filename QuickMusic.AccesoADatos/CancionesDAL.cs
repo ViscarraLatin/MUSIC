c890de820936e60528a29d8e80d2dcdb5a56c28e
Eduardo <sa21002ts@esfe.agape.edu.sv>
2022-09-13 16:29:53 -0600

    Se realizo la conexion ComunDB Contexto y se ha desarrollado DAL.Canciones

--- QuickMusic.AccesoADatos/CancionesDAL.cs
+++ QuickMusic.AccesoADatos/CancionesDAL.cs
@@ -3,10 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QuickMusic.EntidadesDeNegocio;
 
 namespace QuickMusic.AccesoADatos
 {
-    class CancionesDAL
+    public class CancionesDAL
     {
+        public static async Task<int> CrearAsync(Canciones pCanciones)
+        {
+            int result = 0;
+            using (var bdContexto = new DBContexto())
+            {
+                bdContexto.Add(pCanciones);
+                result = await bdContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+        public static async Task<int> ModificarAsync(Canciones pCanciones)
+        {
+            int result = 0;
+            using (var bdContexto = new DBContexto())
+            {
+                var canciones = await bdContexto.Canciones.FirstOrDefaultAsync(s => s.Id == pCanciones.Id);
+                canciones.Id_Artista = pCanciones.Id_Artista;
+                canciones.Id_Genero = pCanciones.Id_Genero;
+                canciones.Titulo = pCanciones.Titulo;
+                bdContexto.Update(canciones);
+                result = await bdContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+        public static async Task<int> EliminarAsync(Canciones pCanciones)
+        {
+            int result = 0;
+            using (var bdContexto = new DBContexto())
+            {
+                var canciones = await bdContexto.Canciones.FirstOrDefaultAsync(s => s.Id == pCanciones.Id);
+
+                bdContexto.Remove(canciones);
+                result = await bdContexto.SaveChangesAsync();
+            }
+            return result;
+        }
+        public static async Task<int> ObtenerPorIdAsync(Canciones pCanciones)
+        {
+            int result = 0;
+            using (var bdContexto = new DBContexto())
+            {
+                var canciones = await bdContexto.Canciones.FirstOrDefaultAsync(s => s.Id == pCanciones.Id);
+            }
+            return result;
+        }
+        public static async Task<List<Canciones>> ObtenerTodosAsync()
+        {
+            var canciones = new List<Canciones>();
+            using (var bdContexto = new DBContexto())
+            {
+                canciones = await bdContexto.Canciones.ToListAsync();
+            }
+            return canciones;
+
+        }
+        internal static IQueryable<Canciones> QuerySelect(IQueryable<Canciones> pQuery, Canciones pCanciones)
+        {
+            if (pCanciones.Id > 0)
+                pQuery = pQuery.Where(s => s.Id == pCanciones.Id);
+            if (pCanciones.Id_Artista > 0)
+                pQuery = pQuery.Where(s => s.Id_Artista == pCanciones.Id_Artista);
+            if (pCanciones.Id_Genero > 0)
+                pQuery = pQuery.Where(s => s.Id == pCanciones.Id_Genero);
+            //if (pCanciones.Titulo > 0)
+            //pQuery = pQuery.Where(s => s.Titulo == pCanciones.Titulo);
+            return pQuery;
+        }
     }
 }
--- QuickMusic.AccesoADatos/DBContexto.cs
+++ QuickMusic.AccesoADatos/DBContexto.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuickMusic.EntidadesDeNegocio;
+using Microsoft.EntityFrameworkCore;
 
 namespace QuickMusic.AccesoADatos
 {
-    class DBContexto
+    public class DBContexto : DbContext
     {
+        public DbSet<Rol> Rol { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Artista> Artista { get; set; }
+        public DbSet<Genero> Genero { get; set; }
+        public DbSet<Canciones> Canciones { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-E348I49E\SQLEXPRESS; Initial Catalog = QuickMusic; Integrated Security=True");
+        }
+
     }
 }
